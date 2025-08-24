78769973cae5753d4ba2d76a01175df6107bf6d2
dimitrisn-nik <dim.nik.public@gmail.com>
2025-08-24 22:57:57 +0300

    Simplified installation process

--- get-imgs.sh
+++ get-imgs.sh
@@ -5,35 +5,46 @@ set -euo pipefail
 REPO_URL="https://github.com/firecracker-microvm/firecracker"
 MOUNT_DIR="mnt"
 ROOTFS_IMG="rootfs.ext4"
+KERNEL_IMG="vmlinux.bin"
+KERNEL_URL="https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin"
+ROOTFS_URL="https://s3.amazonaws.com/spec.ccfc.min/firecracker-ci/v1.12/x86_64/ubuntu-24.04.squashfs"
+INIT_SCRIPT="initialize.sh"
+SERVER_CONFIG="servers.json"
 
-git clone "$REPO_URL"
-./firecracker/tools/devtool build_ci_artifacts rootfs
+echo "[] Getting RootFS image"
+wget -O ubuntu.squashfs $ROOTFS_URL
 
-echo "Filesystem creation done. Now making ext4 img"
-#Copy image
-mv firecracker/resources/x86_64/ubuntu*.squashfs ./
 # Extract 
-unsquashfs -d squashfs-root ubuntu*.squashfs
+echo "[] Extracting image"
+unsquashfs -d squashfs-root ubuntu.squashfs
 #Create ext4 image
-echo "Creating blank img"
-dd if=/dev/zero of=$ROOTFS_IMG bs=1M count=2048
-echo "Formating to ext4"
+echo "[] Creating blank img"
+dd if=/dev/zero of=$ROOTFS_IMG status=progress bs=1M count=2048
+echo "[] Formating to ext4"
 mkfs.ext4 -F $ROOTFS_IMG
 #Mount 
-echo "Mounting img"
+echo "[] Mounting img"
 mkdir -p $MOUNT_DIR
 sudo mount -o loop $ROOTFS_IMG $MOUNT_DIR
 #Copy 
-echo "copying"
+echo "[] Copying"
 sudo cp -a squashfs-root/* $MOUNT_DIR
 
-echo "Fetching kernel from aws CI"
-curl -fsSL -o vmlinux.bin https://s3.amazonaws.com/spec.ccfc.min/img/hello/kernel/hello-vmlinux.bin
-echo "Cleaning up."
+echo "[] Copying initialize.sh"
+sudo cp "$INIT_SCRIPT" "$MOUNT_DIR/root/"
+sudo chmod +x "$MOUNT_DIR/root/$INIT_SCRIPT"
+echo "[] Copying servers.json"
+sudo cp "$SERVER_CONFIG" "$MOUNT_DIR/root/"
+echo "bash /root/initialize.sh" | sudo tee -a $MOUNT_DIR/root/.bashrc
+
+echo "[] Fetching kernel from aws CI"
+curl -fsSL -o $KERNEL_IMG $KERNEL_URL
+echo "[] Cleaning up."
 sudo umount $MOUNT_DIR
 rmdir $MOUNT_DIR
 sudo rm -rf squashfs-root
 rm -rf ubuntu*.squashfs
 
-echo "Done! Rootfs created: $ROOTFS_IMG"
-
+echo "Done! Rootfs: $ROOTFS_IMG"
+echo "Kernel: $KERNEL_IMG"
+python ./start.py
--- initialize.sh
+++ initialize.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
 echo "Configuring network..."
 ip addr add 172.20.0.2/24 dev eth0
 ip link set eth0 up
 ip route add default via 172.20.0.1
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-echo "Installing uv..."
-curl -LsSf https://astral.sh/uv/install.sh | sh
 
 echo "Installing firejail..."
 export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt install -y firejail
+echo "Installing npm"
+apt install -y nodejs npm
+
+echo "Installing uv..."
+apt install --reinstall ca-certificates
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
 
 echo "Installing mcp-proxy..."
 uv tool install mcp-proxy
@@ -22,6 +25,4 @@ echo "Starting mcp-proxy..."
 mcp-proxy \
   --host=0.0.0.0 \
   --port=8080 \
-  --named-server fetch "firejail --noprofile uvx mcp-server-fetch" \
-  --named-server fetch2 "firejail --noprofile uvx mcp-server-fetch"
-
+  --named-server-config /root/servers.json
