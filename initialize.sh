3da618610418a079f1a3d8650c761d198a9d7346
dimitris-nik <dimitrisn2005@gmail.com>
2025-09-29 21:35:26 +0300

    Fixed reboot issue

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
+export PATH="$HOME/.local/bin:$PATH"
 
 echo "Configuring network..."
-ip addr add 172.20.0.2/24 dev eth0
+ip addr replace 172.20.0.2/24 dev eth0
 ip link set eth0 up
-ip route add default via 172.20.0.1
+ip route replace default via 172.20.0.1
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-export PATH="$HOME/.local/bin:$PATH"
 
 FLAG_FILE="/var/lib/first_boot_flag"
 
@@ -36,10 +36,10 @@ if [[ ! -f "$FLAG_FILE" ]]; then
 fi
 
 echo "Scanning Server Configuration"
-mcp-scan ./servers.json
+mcp-scan /root/servers.json
 
 echo "Starting mcp-proxy..."
-mcp-proxy \
+exec mcp-proxy \
   --host=0.0.0.0 \
   --port=8080 \
   --named-server-config /root/servers.json
